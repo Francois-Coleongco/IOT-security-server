0866829e8366ebdbd095cb422f545037d5cb2a3e
Francois Coleongco <christien.coleongco@gmail.com>
2025-07-01 13:25:32 +0900

    FINALLY AUTH WORKS OKAY time for UI and then i'll build the PIR sensor ESP32 thingamabobber

--- src/main/java/Fran/backend_server/config/WebSecurityConfig.java
+++ src/main/java/Fran/backend_server/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).httpBasic(Customizer.withDefaults()).build();
+                .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/token").permitAll().anyRequest().authenticated()).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).httpBasic(Customizer.withDefaults()).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
     }
 
     @Bean
